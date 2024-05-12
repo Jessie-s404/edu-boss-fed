c8dd56cedbdd923c298bbe02f0757682ba5729d7
雨碎风希 <2078389729@qq.com>
2024-05-12 23:58:50 +0800

    element

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,31 @@
 <template>
   <div id="app">
     <h1>Hello World</h1>
-    <p>{{ a }}</p>
-    <button @click="test">测试</button>
     <!-- 根路由出口 -->
-    <router-view/>
+    <router-view />
+    <p class="text">hello World</p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Component from 'vue-class-component'
 
-@Component
-export default class App extends Vue {
-  a = 1
-  b = '2'
-  c = {
-    a: 1,
-    b: '2'
-  }
-
-  test () {
-    console.log(this.a)
-  }
-
-  created () {
-    console.log('app created')
-  }
+interface Foo {
+  a: string
+  b: number
 }
+
+interface Bar { a: string; b: number }
+
+export default Vue.extend({
+  name: 'App'
+})
 </script>
 
-// 1.编辑器给的类型提示
-// 2.TypeScript 编译期间的类型验证
-// export default {
-//   data () {
-//     return {
-//       a: 1,
-//       b: '2',
-//       c: [],
-//       d: {
-//         a: 1,
-//         b: '2'
-//       }
-//     }
-//   },
+<style lang="scss" scoped>
+// @import "~@/styles/variables.scss";
 
-//   methods: {
-//     test() {
-//       this.a.abc()
-//     }
-//   }
-// }
-
-<style lang="scss" scoped></style>
+.text {
+  color: $success-color;
+}
+</style>
