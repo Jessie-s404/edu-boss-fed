925d879d21c30d7b7774f6fb4c115860bd0d45d3
雨碎风希 <2078389729@qq.com>
2024-05-15 23:21:58 +0800

    创建布局

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
     <!-- 根路由出口 -->
     <router-view />
-    <p class="text">hello World</p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import request from '@/utils/request'
 
 export default Vue.extend({
   name: 'App'
 })
 </script>
 
-<style lang="scss" scoped>
-// @import "~@/styles/variables.scss";
-
-.text {
-  color: $success-color;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/layout/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LayoutIndex'
+})
+
+</script>
+
+<style lang="scss" scoped>
+.el-container {
+    min-height: 100vh;
+    min-width: 980px;
+}
+.el-aside {
+    background:#D3DCE6;
+}
+
+.el-header {
+    background:#B3C0D1;
+}
+
+.el-main {
+    background: #E9EEF3;
+}
+</style>
