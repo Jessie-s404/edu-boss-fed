3e4257dacf1722ec288212e0b4cc608ad3a3eac6
雨碎风希 <2078389729@qq.com>
2024-05-23 00:00:07 +0800

    router校验页面访问权限

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Layout from '@/layout/index.vue'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -18,7 +19,10 @@ const routes: Array<RouteConfig> = [
       {
         path: '', // 默认子路由
         name: 'home',
-        component: () => import(/* webpackChunkName: 'home' */ '@/views/home/index.vue')
+        component: () => import(/* webpackChunkName: 'home' */ '@/views/home/index.vue'),
+        meta: {
+          requiresAuth: true // 自定义数据
+        } // meta 默认就是一个空对象
       },
       {
         path: '/advert',
@@ -38,7 +42,10 @@ const routes: Array<RouteConfig> = [
       {
         path: '/role',
         name: 'role',
-        component: () => import(/* webpackChunkName: 'role' */ '@/views/role/index.vue')
+        component: () => import(/* webpackChunkName: 'role' */ '@/views/role/index.vue'),
+        meta: {
+          requiresAuth: true // 自定义数据
+        } // meta 默认就是一个空对象
       },
       {
         path: '/course',
@@ -66,6 +73,36 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   routes
+})
+
+// 全局前置守卫：任何页面的访问都要经过这里
+// to: 要去哪里的路由信息
+// from: 从哪里来的路由信息
+// next: 通行的标志
+router.beforeEach((to, from, next) => {
+  // console.log('进入了全局守卫')
+  console.log('to => ', to)
+  console.log('from => ', from)
+
+  // to.matched 是一个数组（匹配到是路由记录）
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.state.user) {
+      // 跳转到登录页面
+      next({
+        name: 'login'
+      })
+    } else {
+      next() // 允许通过
+    }
+  } else {
+    next() // 允许通过
+  }
+
+  // 路由守卫中一定要调用 next，否则页码无法展示
+  // next()
+  // if (to.path !== '/login') {
+  //   // 校验登录状态
+  // }
 })
 
 export default router
